6eb74d5e63c9e842f5daffee98633fe845acad09
Pham-Bien-Thuy <thuy.pb203601@sis.hust.edu.vn>
2025-06-20 19:53:24 +0700

    done

--- README.md
+++
@@ -1,2 +0,0 @@
-# RISC-V-Pipeline-Processor
-![forwarding_datapath](https://github.com/user-attachments/assets/4a916750-28ed-4706-a6d1-cae09e4aa4a0)
--- utils/macros.v
+++
@@ -1,5 +0,0 @@
-`define assert(sig, val) \
-    if (sig !== val) begin \
-        $display("ASSERTION FAILED in %m: Expected %b, got %b", val, sig); \
-        $finish; \
-    end
